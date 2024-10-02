8da11f6cfcbf27abfcdf760f7c82e21440cac6c0
Mehedi Hasan <mehedihasan62086@gmail.com>
2024-10-02 19:19:08 +0600

    Get the url, login and close

---
+++ SeleniumLoginTraining/src/test/java/ui/LoginWithWebDriver.java
@@ -0,0 +1,22 @@
+package ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginWithWebDriver {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.saucedemo.com/v1/");
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+		driver.close();
+
+	}
+
+}
